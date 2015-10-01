8a604c8c6527a3c0986efc3817d241d8c1235ba0
AFranceschini <antoinefranceschini@MacBook-Air-de-Antoine.local>
2015-10-01 15:13:10 +0200

    Test
    Test

---
+++ config.js
@@ -0,0 +1,17 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'test-wp',
+		wp_ws_url : 'http://vps78594.ovh.net/mobile/wp-appkit-api/test-wp',
+		theme : 'nihao-ios',
+		version : '1.0.0',
+		app_title : 'Application Test',
+		debug_mode : 'off',
+		auth_key : '^dFk!qL4mrJ!bTBK@Z!ESguP87JIoiHacXG(WpjtrsWpSvSzof*hy(@8MClMhueM',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
